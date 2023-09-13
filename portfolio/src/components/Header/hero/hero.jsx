7eb3bb9244488d07f521d2ef10bb04b2b095597c
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2023-09-13 19:25:13 +0700

    Update

--- portfolio/src/components/Header/hero/hero.jsx
+++ portfolio/src/components/Header/hero/hero.jsx
@@ -7,14 +7,37 @@ const Hero = () => {
       <div className={`innerWidth ${css.container}`}>
         <div className={css.upperElements}>
           <span>
-            Hey there, <br/>I am Nhan
+            Hey there, <br />I am Nhan
           </span>
           <span>
             I design beautiful simple
-            <br/>
+            <br />
+            things, And I love what I do
           </span>
         </div>
-        <div className={css.lowerElements}></div>
+
+        <div className={css.person}>
+          <img src="./person.png" alt="" />
+        </div>
+
+        <a className={css.email} href="mailto:nhantran4102002@gmail.com" target="_blank">
+          nhantran4102002@gmail.com
+        </a>
+
+        <div className={css.lowerElements}>
+          <div className={css.experience}>
+            <div className="primaryText">10</div>
+            <div className="secondaryText">
+              <div>Years</div>
+              <div>Expriences</div>
+            </div>
+          </div>
+          <div className={css.certification}>
+            <img src="./certificate.png" alt="" />
+            <span>CERTIFIED PROFATIONAL</span>
+            <span>UI/UX DESIGNER</span>
+          </div>
+        </div>
       </div>
     </section>
   );
