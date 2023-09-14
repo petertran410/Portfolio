c93988f744718ea8560e422550218f58bea82171
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2023-09-14 19:02:49 +0700

    Update

--- portfolio/src/components/Experties/experties.jsx
+++ portfolio/src/components/Experties/experties.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import css from "./experties.module.scss";
+import { projectExperience } from "../../utils/data";
 
 const Experties = () => {
   return (
     <section className={css.wrapper}>
       <div
         className={`paddings yPaddings flexCenter innerWidth ${css.container}`}>
-        <div className={css.leftSide}>Ngọc Nhân</div>
-        <div className={css.rightSide}>Ngọc Nhân</div>
+        <div className={css.leftSide}>
+          {projectExperience.map((exp, i) => {
+            return (
+              <div className={css.exp} key={i}>
+                <div className="flexCenter">
+                  <exp.icon size={25} color="red" />
+                </div>
+
+                <div>
+                  <span>{exp.name}</span>
+                  <span>{exp.projects}</span>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className={css.rightSide}>right side</div>
       </div>
     </section>
   );
