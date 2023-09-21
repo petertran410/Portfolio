c18f46509a740705096ffd61aac7443348551b44
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2023-09-21 14:32:30 +0700

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Experties from "./assets/components/Experties/experties";
+import Experties from "./assets/Pages/Experties/experties";
 import Header from "./assets/components/Header/header";
 import Hero from "./assets/components/Header/Hero/hero";
-import Portfolio from "./assets/components/Portfolio/portfolio";
-import Works from "../src/assets/components/Works/works";
+import Portfolio from "./assets/Pages/Portfolio/portfolio";
+import Works from "../src/assets/Pages/Works/works";
 import css from "./styles/app.module.scss";
 
 const App = () => {
